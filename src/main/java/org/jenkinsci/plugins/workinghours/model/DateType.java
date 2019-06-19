4685572cf1de832f0ebbb80dc7e764da8f873719
Jeff Pearce <jxpearce@godaddy.com>
2019-06-19 10:46:14 -0700

    Temp disable certain findbugs warnings (#15)

--- src/main/java/org/jenkinsci/plugins/workinghours/model/DateType.java
+++ src/main/java/org/jenkinsci/plugins/workinghours/model/DateType.java
@@ -1,6 +1,6 @@
 package org.jenkinsci.plugins.workinghours.model;
 
 public class DateType {
-    public static String TYPE_GREGORIAN = "Gregorian";
-    public static String TYPE_CHINESE_LUNAR = "Chinese Lunar";
+    public final static String TYPE_GREGORIAN = "Gregorian";
+    public final static String TYPE_CHINESE_LUNAR = "Chinese Lunar";
 }
