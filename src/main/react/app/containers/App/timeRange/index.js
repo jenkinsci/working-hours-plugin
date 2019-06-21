e1456aa2f8b32543085f811f938bc45f0eb0347a
sjjkk <357875929@qq.com>
2019-06-22 01:20:27 +0800

    Customize time range slider.

--- src/main/react/app/containers/App/timeRange/index.js
+++ src/main/react/app/containers/App/timeRange/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { WEEKDAYS } from "../constants";
 import moment from "moment";
+
 import TimePicker from "rc-time-picker";
+import "../style/components.css"
 import "rc-time-picker/assets/index.css";
 import "rc-slider/assets/index.css";
-import Slider, { createSliderWithTooltip, Range } from "rc-slider";
+import Slider, { createSliderWithTooltip, Range,Handle } from "rc-slider";
+import Tooltip from "rc-tooltip"
 
-const timeRegExp = /^(?:(?:[0-2][0-3])|(?:[0-1][0-9])):[0-5][0-9]$/;
+const timeRegExp = /^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])$/;
 
 /**
  * Convert number of minutes to string like '00:00'.
@@ -33,7 +36,7 @@ function timeStringToMinutes(timeString) {
 
 const SliderWithTooltip = createSliderWithTooltip(Slider);
 
-export default class Index extends React.Component {
+export default class TimeRange extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -42,7 +45,7 @@ export default class Index extends React.Component {
       endTime: 18 * 60,
 
       /*Strings used for store temp times, which would be checked on blur.*/
-      tempStartTime: "8:00",
+      tempStartTime: "08:00",
       tempEndTime: "18:00",
 
       /*Symbol that indicates whether time is valid.*/
@@ -54,38 +57,41 @@ export default class Index extends React.Component {
 
   updateStartTime = (e) => {
     this.setState({
-      tempStartTime: e.target.value
+      tempStartTime: e.target.value.trim()
     });
   };
 
   updateEndTime = (e) => {
     this.setState({
-      tempEndTime: e.target.value
+      tempEndTime: e.target.value.trim()
     });
   };
 
+  /**
+   * Process time after the input blur.
+   */
   handleInputBlur = () => {
     /*Test input format*/
     if (timeRegExp.test(this.state.tempStartTime)) {
       this.setState({
         startTime: timeStringToMinutes(this.state.tempStartTime),
+        tempStartTime: timeFormatter(timeStringToMinutes(this.state.tempStartTime)),
         validStartTime: true
       });
     } else {
       this.setState({
-        tempEndTime: timeStringToMinutes(this.state.tempStartTime),
-        validEndTime: false
+        validStartTime: false
       });
     }
 
     if (timeRegExp.test(this.state.tempEndTime)) {
       this.setState({
-        tempEndTime: timeStringToMinutes(this.state.tempStartTime),
+        endTime: timeStringToMinutes(this.state.tempEndTime),
+        tempEndTime: timeFormatter(timeStringToMinutes(this.state.tempEndTime)),
         validEndTime: true
       });
     } else {
       this.setState({
-        tempEndTime: timeStringToMinutes(this.state.tempStartTime),
         validEndTime: false
       });
     }
@@ -108,9 +114,9 @@ export default class Index extends React.Component {
     this.props.onEdit(this.props.index, this.state);
   };
 
-  validate() {
+  validate = () => {
     return this.state.validEndTime && this.state.validStartTime;
-  }
+  };
 
   handleRangeChange = (range) => {
     this.setState({
@@ -123,7 +129,6 @@ export default class Index extends React.Component {
     });
   };
 
-
   render() {
     return (
       <div className={["time-range", this.validate() ? "" : "time-range-invalid"].join(" ")}
@@ -132,16 +137,28 @@ export default class Index extends React.Component {
           {Object.keys(WEEKDAYS).map(key => <option value={WEEKDAYS[key]} key={key}>{key}</option>)}
         </select>
 
-        <input value={this.state.tempStartTime} className={"input input-text"} style={{ width: 70 }}
+        <input value={this.state.tempStartTime} className={"input input-text"} style={{ width: 70, marginLeft: 10 }}
                onChange={this.updateStartTime}
                onBlur={this.handleInputBlur}
         />
         <Range
           style={{ margin: 10 }}
-          max={1440}
+          max={1439}
           min={0}
-          tipProps={{ overlayClassName: "foo" }}
-          defaultValue={[this.state.startTime, this.state.endTime]}
+          step={30}
+          trackStyle={[{ backgroundColor: "#b1b1b1" }, { backgroundColor: "#b1b1b1" }]}
+          handleStyle={[
+            {
+              boxShadow: "0 0 5px #868686",
+              backgroundColor: "white",
+              border: "4px solid rgb(200, 200, 200)"
+            },
+            {
+              boxShadow: "0 0 5px #868686",
+              backgroundColor: "white",
+              border: "4px solid rgb(200, 200, 200)"
+            }]}
+          value={[this.state.startTime, this.state.endTime]}
           onChange={this.handleRangeChange}
 
         >
