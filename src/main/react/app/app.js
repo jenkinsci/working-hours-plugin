a29394de37a69dbf5c6e1963b96d8976e07bc10b
sjjkk <357875929@qq.com>
2019-07-16 23:10:10 +0800

    remove moment-timezone/dates-holiday

--- src/main/react/app/app.js
+++ src/main/react/app/app.js
@@ -10,22 +10,6 @@ import ReactDOM from "react-dom";
 import "sanitize.css/sanitize.css";
 // Import root app
 import App from "containers/App";
-import moment from 'moment-timezone';
-
-window.Holidays = require('date-holidays')
-
-var now = new Date();
-var momentFromDate = moment(now);
-var utcDateTime = momentFromDate.utc();
-var dateTimeString = utcDateTime.format();
-var momentFromString = moment(dateTimeString);
-
-
-// console.log('UTC TIME: ' + dateTimeString);
-//
-// console.log('LA TIME (from moment object): ' + utcDateTime.tz('America/Los_Angeles').format());
-// console.log('LA TIME (from ' + dateTimeString + '): ' + momentFromString.tz('America/Los_Angeles').format());
-
 
 const MOUNT_NODE = document.getElementById("container-excluded-dates");
 const render = () => {
