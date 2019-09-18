7cf752e8d4f8f57dda1adcde608ac2233cc6f70b
Jeff Pearce <jeffpea@gmail.com>
2019-09-18 07:16:09 -0700

    Fix build error

--- src/main/java/org/jenkinsci/plugins/workinghours/WorkingHoursQueueTaskDispatcher.java
+++ src/main/java/org/jenkinsci/plugins/workinghours/WorkingHoursQueueTaskDispatcher.java
@@ -90,6 +90,7 @@ public class WorkingHoursQueueTaskDispatcher extends QueueTaskDispatcher {
                             @Override
                             public String getShortDescription() {
                                 return WorkingHoursConfig.get().getJobHoldText();
+                            }
                         }
                     }
                 }
